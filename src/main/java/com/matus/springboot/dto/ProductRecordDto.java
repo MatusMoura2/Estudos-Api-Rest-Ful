cdf3de6a1ad29e263a74bcd44c1eb4c170320224
Matus Moura <matus.moura333x2@gmail.com>
2024-02-22 21:55:50 -0300

    criação do Record ProductRecordDto, craçaõ do metodo crud post

---
+++ src/main/java/com/matus/springboot/dto/ProductRecordDto.java
@@ -0,0 +1,10 @@
+package com.matus.springboot.dto;
+
+import java.math.BigDecimal;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record ProductRecordDto(@NotBlank String name, @NotNull BigDecimal value) {
+
+}
--- src/main/java/com/matus/springboot/repositories/ProductRepository.java
+++ src/main/java/com/matus/springboot/repositories/ProductRepository.java
@@ -3,9 +3,11 @@ package com.matus.springboot.repositories;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.matus.springboot.models.ProductModel;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductModel, UUID>{
 
 }

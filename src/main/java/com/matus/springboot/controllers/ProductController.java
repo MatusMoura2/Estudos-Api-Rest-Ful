bad04bb4aed0719e8335610ae87a183723abaf6c
Matus Moura <matus.moura333x2@gmail.com>
2024-02-26 11:24:02 -0300

    Implementando metodo getAll

--- src/main/java/com/matus/springboot/controllers/ProductController.java
+++ src/main/java/com/matus/springboot/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package com.matus.springboot.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,5 +30,9 @@ public class ProductController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
 	}
 	
+	@GetMapping("/products")
+	public ResponseEntity<List<ProductModel>> getAllProducts(){
+		return ResponseEntity.status(HttpStatus.OK).body(productRepository.findAll());
+	}
 	
 }

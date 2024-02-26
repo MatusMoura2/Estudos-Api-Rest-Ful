0fd776629ad5273bb216f7dc6ab5ca738e825a6e
Matus Moura <matus.moura333x2@gmail.com>
2024-02-26 14:07:33 -0300

    Metodo delete

--- src/main/java/com/matus/springboot/controllers/ProductController.java
+++ src/main/java/com/matus/springboot/controllers/ProductController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,5 +59,15 @@ public class ProductController {
 		BeanUtils.copyProperties(productRecordDto, productModel);
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(productRepository.save(productModel));
 	}
-
+	
+	@DeleteMapping("/products/{id}")
+	public ResponseEntity<Object> deleteProduct(@PathVariable(value="id") UUID id, @RequestBody @Valid ProductRecordDto productRecordDto){
+		Optional<ProductModel> product0 = productRepository.findById(id);
+		if(product0.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body("produto não encontrado.");
+		}
+		
+		productRepository.delete(product0.get());
+		return ResponseEntity.status(HttpStatus.OK).body("Produto deletado com sucesso.");
+	}
 }
